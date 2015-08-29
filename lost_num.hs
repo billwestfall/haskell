7d83d576a76f40bd89fa4e6006ec09f15ab86f95
billwestfall <billwestfall@yahoo.com>
2015-08-29 11:51:35 -0700

    Create lost_num.hs

---
+++ lost_num.hs
@@ -0,0 +1,13 @@
+{- from http://learnyouahaskell.com/starting-out#ready-set-go
+run as
+*Main> :l lost_num.hs
+[1 of 1] Compiling Main             ( lost_num.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> lostNumbers
+[4,8,15,16,23,42]
+*Main> foundNumbers ++ lostNumbers
+[3,9,16,17,24,43,4,8,15,16,23,42]
+-}
+
+lostNumbers = [4,8,15,16,23,42]
+foundNumbers = [3,9,16,17,24,43]
