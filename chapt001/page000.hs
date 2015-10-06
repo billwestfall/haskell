6a8c6a12686a8a036b7aa21e8df3976121004070
billwestfall <billwestfall@yahoo.com>
2015-10-05 19:01:47 -0700

    Create page000.hs

---
+++ chapt001/page000.hs
@@ -0,0 +1,13 @@
+{-
+Prelude> :l page000.hs
+[1 of 1] Compiling Main             ( page000.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> main
+Hello, World!
+*Main> x
+8
+*Main>
+-}
+
+main = putStrLn "Hello, World!"
+x = 5+3
