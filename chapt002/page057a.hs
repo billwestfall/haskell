47ce2f10f5fe7aff2c09fedb9c2c91d9a2edc8af
billwestfall <billwestfall@yahoo.com>
2015-10-08 18:51:07 -0700

    Create page057a.hs

---
+++ chapt002/page057a.hs
@@ -0,0 +1,15 @@
+{-
+*Main> :l page057a.hs
+[1 of 1] Compiling Main             ( page057a.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> plus 2 3
+5
+*Main> times 3 4
+12
+*Main> treble 5
+15
+-}
+
+plus m n = m + n
+times m n = m * n
+treble m = 3 * m
