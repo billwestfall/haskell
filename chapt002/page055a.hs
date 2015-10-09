3893d10556e566657f0757b3d2c5a423e98907f2
billwestfall <billwestfall@yahoo.com>
2015-10-08 18:22:02 -0700

    Create page055a.hs

---
+++ chapt002/page055a.hs
@@ -0,0 +1,12 @@
+{-
+Prelude> :l page055a.hs
+[1 of 1] Compiling Main             ( page055a.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> double 3.1
+6.2
+*Main> triple 3.2
+9.600000000000001
+-}
+
+double x = x + x
+triple x = 3 * x
