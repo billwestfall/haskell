60555b6a46eff0e5eed610682def36f7218d012a
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 21:08:48 -0700

    Update 007.hs

--- 04chapt/007.hs
+++ 04chapt/007.hs
@@ -1,2 +1,13 @@
+{-
+MacBook-Air:04chapt billw$ ghci 007.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 007.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> second (2, 3, 4)
+3
+*Main> second (9, 7, 8)
+7
+-}
+
 second :: (a, b, c) -> b  
 second (_, y, _) = y
