a40f40bccbc79fcc49c058cc4e5d6e95919a4216
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 19:41:07 -0700

    Update 002.hs

--- 03chapt/002.hs
+++ 03chapt/002.hs
@@ -1,2 +1,12 @@
+{-
+MacBook-Air:03chapt billw$ ghci 002.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 002.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> addThree 5 6 7
+18
+*Main> 
+-}
+
 addThree :: Int -> Int -> Int -> Int  
 addThree x y z = x + y + z
