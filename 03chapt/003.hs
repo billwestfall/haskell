bc0c679fa63669c9cef63d8e40670df89643c32e
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 19:45:44 -0700

    Update 003.hs

--- 03chapt/003.hs
+++ 03chapt/003.hs
@@ -1,2 +1,14 @@
+{-
+MacBook-Air:03chapt billw$ ghci 003.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 003.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> removeNonUppercase "AbCdEfGhIj"
+"ACEGI"
+*Main> removeNonUppercase "zYxWvUtS"
+"YWUS"
+*Main> 
+-}
+
 removeNonUppercase :: [Char] -> [Char]  
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']] 
