d72d28690e3b379a7a958d4d9889d636a132b1d6
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 20:53:23 -0700

    Update 005.hs

--- 04chapt/005.hs
+++ 04chapt/005.hs
@@ -1,3 +1,17 @@
+{- did not get this working here is the error
+MacBook-Air:04chapt billw$ ghci 005.hs -XFlexibleContexts
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 005.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> addVectors 5 6
+
+<interactive>:2:1:
+    No instance for (Num (a0, a0)) arising from a use of ‘it’
+    In the first argument of ‘print’, namely ‘it’
+    In a stmt of an interactive GHCi command: print it
+*Main>
+-}
+
 {-# OPTIONS -XFlexibleContexts #-}
 
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
