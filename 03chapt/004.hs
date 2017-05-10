f831665c7ea0b6836636c532e31944c2f6f8e73b
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 19:48:11 -0700

    Update 004.hs

--- 03chapt/004.hs
+++ 03chapt/004.hs
@@ -1,2 +1,14 @@
+{-
+MacBook-Air:03chapt billw$ ghci 004.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 004.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> factorial 3
+6
+*Main> factorial 5
+120
+*Main>
+-}
+
 factorial :: Integer -> Integer  
 factorial n = product [1..n] 
