4a56901cb6ef3cdef6a2cc139331131f2df9727c
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 19:32:03 -0700

    Create 001.hs

---
+++ 03chapt/001.hs
@@ -0,0 +1,10 @@
+ghci> :t 'a'  
+'a' :: Char  
+ghci> :t True  
+True :: Bool  
+ghci> :t "HELLO!"  
+"HELLO!" :: [Char]  
+ghci> :t (True, 'a')  
+(True, 'a') :: (Bool, Char)  
+ghci> :t 4 == 5  
+4 == 5 :: Bool 
