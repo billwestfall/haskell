2a39d6533db7f35f834d81d0f9626f685e1926d7
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 21:19:07 -0700

    Update 008.hs

--- 04chapt/008.hs
+++ 04chapt/008.hs
@@ -1,2 +1,13 @@
+{-
+MacBook-Air:04chapt billw$ ghci 008.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 008.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> third (5, 6, 7)
+7
+*Main> third (9, 8, 1)
+1
+-}
+
 third :: (a, b, c) -> c  
 third (_, _, z) = z
