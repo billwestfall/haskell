22e8a89567f9f9b90cf94cdc2900b92bf16270fa
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 20:22:55 -0700

    Update 004.hs

--- 04chapt/004.hs
+++ 04chapt/004.hs
@@ -1,3 +1,15 @@
+{-
+MacBook-Air:04chapt billw$ ghci 004.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 004.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> charName 'c'
+"Cecil"
+*Main> charName 'a'
+"Albert"
+*Main>
+-}
+
 charName :: Char -> String  
 charName 'a' = "Albert"  
 charName 'b' = "Broseph"  
