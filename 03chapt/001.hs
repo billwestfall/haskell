2791684ccabe2fc55cc4eb016ef39c40df9c1fa4
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 19:37:25 -0700

    Update 001.hs

--- 03chapt/001.hs
+++ 03chapt/001.hs
@@ -1,5 +1,15 @@
-:t 'a'  
-:t True  
-:t "HELLO!"  
-:t (True, 'a')  
-:t 4 == 5  
+MacBook-Air:03chapt billw$ ghci
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+Prelude> :t 'a'
+'a' :: Char
+Prelude> :t True
+True :: Bool
+Prelude> :t "HELLO!"
+"HELLO!" :: [Char]
+Prelude> :t (True, 'a')
+(True, 'a') :: (Bool, Char)
+Prelude> :t 4 ==5
+4 ==5 :: Bool
+Prelude> :t 4 == 5
+4 == 5 :: Bool
+Prelude>
