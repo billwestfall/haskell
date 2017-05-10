66d08c7cfc919d729f772fef0bb550d310bfc32b
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 21:01:10 -0700

    Update 006.hs

--- 04chapt/006.hs
+++ 04chapt/006.hs
@@ -1,2 +1,14 @@
+{-
+MacBook-Air:04chapt billw$ ghci 006.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 006.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> first (3, 2, 1)
+3
+*Main> first (7, 9, 8)
+7
+*Main>
+-}
+
 first :: (a, b, c) -> a  
 first (x, _, _) = x
