831bf34bd22c1d7283240938a2960b021cc130bb
Bill Westfall <billwestfall@yahoo.com>
2017-05-09 20:10:09 -0700

    Update 002.hs

--- 04chapt/002.hs
+++ 04chapt/002.hs
@@ -1,3 +1,16 @@
+{-
+MacBook-Air:04chapt billw$ ghci 002.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 002.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> sayMe 4
+"Four!"
+*Main> sayMe 9
+"Not between 1 and 5"
+*Main> sayMe 3
+"Three!"
+-}
+
 sayMe :: (Integral a) => a -> String 
 sayMe 1 = "One!"  
 sayMe 2 = "Two!"  
