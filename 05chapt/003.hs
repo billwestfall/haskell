6c927e75562b3413a42f906d95ae61d14d76769e
Bill Westfall <billwestfall@yahoo.com>
2017-05-13 11:01:05 -0700

    Update 003.hs

--- 05chapt/003.hs
+++ 05chapt/003.hs
@@ -1,3 +1,18 @@
+{-
+MacBook-Air:05chapt billw$ ghci 003.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 003.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> take' 3 [1,2,3]
+[1,2,3]
+*Main> take' 3 [1,2,3,4]
+[1,2,3]
+*Main> take' 3 [1,2,3,4,5]
+[1,2,3]
+*Main> take' 2 [5,4,3,2,1]
+[5,4]
+-}
+
 take' :: (Num i, Ord i) => i -> [a] -> [a]  
 take' n _  
     | n <= 0   = []  
