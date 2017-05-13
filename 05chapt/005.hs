4eab1401836ecb0d63afd75a058822ffef921a35
Bill Westfall <billwestfall@yahoo.com>
2017-05-13 11:15:40 -0700

    Update 005.hs

--- 05chapt/005.hs
+++ 05chapt/005.hs
@@ -1,2 +1,11 @@
+{-
+MacBook-Air:05chapt billw$ ghci 005.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 005.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> repeat 5
+[5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,....
+-}
+
 repeat' :: a -> [a]  
 repeat' x = x:repeat' x
