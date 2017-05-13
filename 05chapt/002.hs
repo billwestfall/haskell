3e54542e37775924dfae7f1f506f10d0fa8a98a3
Bill Westfall <billwestfall@yahoo.com>
2017-05-13 10:52:00 -0700

    Update 002.hs

--- 05chapt/002.hs
+++ 05chapt/002.hs
@@ -1,3 +1,14 @@
+{-
+MacBook-Air:05chapt billw$ ghci 002.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 002.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> replicate' 5 2
+[2,2,2,2,2]
+*Main> replicate' 2 5
+[5,5]
+-}
+
 replicate' :: (Num i, Ord i) => i -> a -> [a]  
 replicate' n x  
     | n <= 0    = []  
