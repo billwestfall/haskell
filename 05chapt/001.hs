8a4793a8235a710a8ae66d745a4b72a7f67ce446
Bill Westfall <billwestfall@yahoo.com>
2017-05-13 10:43:16 -0700

    Update 001.hs

--- 05chapt/001.hs
+++ 05chapt/001.hs
@@ -1,3 +1,14 @@
+{-
+MacBook-Air:05chapt billw$ ghci 001.hs 
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( 001.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> maximum'[2,4,5]
+5
+*Main> maximum'[11, 1, 2]
+11
+-}
+
 maximum' :: (Ord a) => [a] -> a  
 maximum' [] = error "maximum of empty list"  
 maximum' [x] = x  
