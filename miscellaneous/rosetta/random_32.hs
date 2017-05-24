1d6d5a1d112f49558fbd6240052de2abde283f58
Bill Westfall <billwestfall@yahoo.com>
2017-05-23 17:42:59 -0700

    Update random_32.hs

--- miscellaneous/rosetta/random_32.hs
+++ miscellaneous/rosetta/random_32.hs
@@ -1,4 +1,8 @@
 {- run like the following
-import System.Random
-randomRIO (0, 2147483647)
+MacBook-Air:rosetta billw$ ghci
+GHCi, version 7.10.2: http://www.haskell.org/ghc/  :? for help
+Prelude> import System.Random
+Prelude System.Random> randomRIO (0, 2147483647)
+1281092829
+Prelude System.Random>
 -}
