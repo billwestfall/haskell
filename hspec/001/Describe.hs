d14b9890db9512bd12af7b3561ead76818ba73ae
Bill Westfall <billwestfall@yahoo.com>
2018-03-16 21:47:32 -0700

    Create Describe.hs

---
+++ hspec/001/Describe.hs
@@ -0,0 +1,8 @@
+main :: IO ()
+main = hspec $ do
+  describe "Prelude.read" $ do
+    it "can parse integers" $ do
+      read "10" `shouldBe` (10 :: Int)
+
+    it "can parse floating-point numbers" $ do
+      read "2.5" `shouldBe` (2.5 :: Float)
