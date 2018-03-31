49811811a66f7cb571b57134f7967e194ae960aa
Bill Westfall <billw@concur.com>
2018-03-30 21:56:25 -0700

    Fix line break

--- 11chapt/001.hs
+++ 11chapt/001.hs
@@ -1,8 +1,9 @@
-import Test.QuickCheck 
+import Test.QuickCheck
 import Data.List
 
 qsort :: Ord a => [a] -> [a]
 qsort [] = []
 qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
-where lhs = filter (< x) xs rhs = filter (>= x) xs
+  where lhs = filter (< x) xs
+        rhs = filter (>= x) xs
 prop_idempotent xs = qsort (qsort xs) == qsort xs
