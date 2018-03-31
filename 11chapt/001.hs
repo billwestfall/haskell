d933bb56ec9d21c8ff76c0e31decad644fbdab77
Bill Westfall <billwestfall@yahoo.com>
2018-03-30 22:00:48 -0700

    Update 001.hs

--- 11chapt/001.hs
+++ 11chapt/001.hs
@@ -7,6 +7,7 @@ qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
   where lhs = filter (< x) xs
         rhs = filter (>= x) xs
 prop_idempotent xs = qsort (qsort xs) == qsort xs
+putStrLn xs
 
 main :: IO ()
-main = return (xs)
+main = return ()
