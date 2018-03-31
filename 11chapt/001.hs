6a48e5041100f1132e784e9256e5c2408aff19fa
Bill Westfall <billwestfall@yahoo.com>
2018-03-30 22:02:42 -0700

    Update 001.hs

--- 11chapt/001.hs
+++ 11chapt/001.hs
@@ -1,13 +1,11 @@
 import Test.QuickCheck
 import Data.List
 
+main = do
 qsort :: Ord a => [a] -> [a]
 qsort [] = []
 qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
   where lhs = filter (< x) xs
         rhs = filter (>= x) xs
 prop_idempotent xs = qsort (qsort xs) == qsort xs
-putStrLn xs
-
-main :: IO ()
-main = return ()
+  main
