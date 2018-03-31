2ae27e38cf399d98530af3f0acd73314b772acd0
Bill Westfall <billwestfall@yahoo.com>
2018-03-30 22:25:31 -0700

    Create minimum.hs

---
+++ 11chapt/minimum.hs
@@ -0,0 +1,8 @@
+head
+head (x:_) head []
+minimum minimum minimum
+:: [a] -> a
+= x
+= error "Prelude.head: empty list"
+:: (Ord a) => [a] -> a
+= error "Prelude.minimum: empty list" = foldl1 min xs
